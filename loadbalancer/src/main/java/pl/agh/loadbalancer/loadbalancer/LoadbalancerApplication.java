2846629074a62493d33983dcaf010d52ef4d9d97
lstachon <64076091+lstachon@users.noreply.github.com>
2021-12-16 16:35:09 +0100

    server

--- loadbalancer/src/main/java/pl/agh/loadbalancer/loadbalancer/LoadbalancerApplication.java
+++ loadbalancer/src/main/java/pl/agh/loadbalancer/loadbalancer/LoadbalancerApplication.java
@@ -2,12 +2,17 @@ package pl.agh.loadbalancer.loadbalancer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.agh.loadbalancer.loadbalancer.socketServer.SocketServer;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class LoadbalancerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        SocketServer server = new SocketServer();
+        server.startSocketServer();
         SpringApplication.run(LoadbalancerApplication.class, args);
 
     }
