8870f697347fe678e75637c464e23524160f784b
youngdashu <anowak2000@gmail.com>
2022-01-27 19:13:50 +0100

    QueryProcessor if else gitignore

--- loadbalancer/src/main/java/pl/agh/dp/loadbalancer/DataBaseInstance/QueryProcessor/QueryProcessor.java
+++ loadbalancer/src/main/java/pl/agh/dp/loadbalancer/DataBaseInstance/QueryProcessor/QueryProcessor.java
@@ -1,9 +1,13 @@
 package pl.agh.dp.loadbalancer.DataBaseInstance.QueryProcessor;
 
 import lombok.Setter;
+import org.hibernate.HibernateException;
+import org.hibernate.query.Query;
 import pl.agh.dp.loadbalancer.DataBaseInstance.DataBaseInstance;
 import pl.agh.dp.loadbalancer.DataBaseInstance.DataBaseStates;
 import pl.agh.dp.loadbalancer.command.Command;
+
+import org.hibernate.Session;
 
 public class QueryProcessor<T extends Command> implements Runnable {
 
@@ -40,14 +44,35 @@ public class QueryProcessor<T extends Command> implements Runnable {
             T commandToProcess = this.queryQueue.get();
 
             if(databaseInstance.getState().equals(DataBaseStates.CONNECTED)){
-                
+                // wysylamy wszystko
+
+
+            } else if(databaseInstance.getState().equals(DataBaseStates.RESTORING)){
+                // odbiermay wszystko, wysyłamy dmle
+
+
+
+            } else{
+                // nie pobiera
+
+
             }
 
             String commandAsString = commandToProcess.getCommand();
 
             // send to db
 
+            Session databaseSession =  databaseInstance.getSession();
 
+            Query resultQuery;
+
+            try{
+                resultQuery = databaseSession.createQuery(commandAsString);
+            } catch (HibernateException exception){
+                System.out.println(exception.toString());
+            }
+
+            
 
         }
     }
