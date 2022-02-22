d6e1886fe6c95d37e38ffbb997ad10cb722e3927
lstachon <64076091+lstachon@users.noreply.github.com>
2022-02-22 21:07:07 +0100

    mapping

---
+++ client/src/main/java/pl/agh/dp/loadbalancer/main.java
@@ -0,0 +1,14 @@
+package pl.agh.dp.loadbalancer;
+
+import pl.agh.dp.loadbalancer.Employee.EmployeeEntity;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+
+public class main {
+    public static void main(String[] args) {
+
+
+    }
+}
